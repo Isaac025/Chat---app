c68ccab2a1ba257f63767ef05bce68e93bb27b14
Isaac025 <isaacolayiwola85@gmail.com>
2026-01-06 16:05:50 +0100

    first commit

---
+++ client/src/lib/utils.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function formatMessageTime(date) {
+  return new Date(date).toLocaleTimeString("en-US", {
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: false,
+  });
+}
